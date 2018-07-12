ae2e8a7d968097523d34ccfc514920719f00cbcc
Sandeep Alla <A1821331@IIRCGA255>
2018-07-12 18:37:03 -0500

    Updates

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var app = express();
 var appRoutes = require('./routes/appRoutes');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 mongoose.connect('mongodb://localhost:27017/DocumentsArena', {useNewUrlParser: true });
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/', appRoutes);
